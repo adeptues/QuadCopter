acc9219ac75d06c57051c9ce1f891c5f7e2b08ca
Thomas Helmkay <adeptues@googlemail.com>
2013-09-29 16:26:47 +0100

    export path command

---
+++ exportpaths.sh
@@ -0,0 +1,4 @@
+#Need to export the libs dir to tell the executable where they are
+#replace the path with your system path to the lib folder inside the project
+
+export LD_LIBRARY_PATH="/home/adeptues/QuadCopter/external/linux64/lib"
